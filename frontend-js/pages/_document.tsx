cbeb45eec2e6104454b76334a6ecb6e5bc09887e
Walter Teng <16046667+davzoku@users.noreply.github.com>
2023-08-17 16:36:46 +0800

    add GA

--- frontend-js/pages/_document.tsx
+++ frontend-js/pages/_document.tsx
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import Script from 'next/script';
+import GoogleAnalytics from "@bradgarropy/next-google-analytics"
 
 export default function Document() {
   return (
@@ -7,6 +8,18 @@ export default function Document() {
       <Head>
         <link rel="manifest" href="/manifest.json" />
         <title>CRIA: Your Friendly Baby Llama Chatbot</title>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+          });
+          `,
+        }}
+        />
       </Head>
       <body suppressHydrationWarning={true}>
         <Main />
